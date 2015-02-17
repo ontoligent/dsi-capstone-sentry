49ece96c7990c529a8ad04ba53f7b975791b5f8d
Raf Alvarado <ontoligent@gmail.com>
2015-02-17 13:52:27 -0500

    Added getlines sub to scraper

--- scraper.pl
+++ scraper.pl
@@ -5,9 +5,8 @@ use strict;
 
 sub download_daily_docs {
   my $url1 = "http://www.sec.gov/cgi-bin/current.pl?q1=0&q2=0&q3=";
-  my $doc1 = get($url1);
-  my @doc1 = split /\n/, $doc1;
-  for my $line1 (@doc1) {
+  my @lines1 = &getlines($url1);
+  for my $line1 (@lines1) {
     if ($line1 =~ /<a href="\/(Archives\/edgar\/data\/\d+\/(\d+-\d+-\d+)-index.html)">10-K/) {
       my $id = $2;
       my $tenkfile = "docs-10k/${id}_10k.html";
@@ -16,9 +15,8 @@ sub download_daily_docs {
         next;
       }
       my $url2 = "http://www.sec.gov/$1";
-      my $doc2 = get($url2);
-      my @doc2 = split /\n/, $doc2;
-      for my $line2 (@doc2) {
+      my @lines2 = &getlines($url2);
+      for my $line2 (@lines2) {
         if ($line2 =~ /<td scope="row"><a href="(\/Archives\/edgar\/data\/\d+\/\d+\/.+10k.htm)"/) {
           my $url = "http://www.sec.gov$1";
           print "Creating $tenkfile\n";
@@ -28,4 +26,11 @@ sub download_daily_docs {
       }      
     }
   }
+}
+
+sub getlines {
+  my $url = shift;
+  my $doc = get($url);
+  my @lines = split /\n/, $doc;
+  return @lines;
 }
